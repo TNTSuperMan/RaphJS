036ca104e8ace7cc70fca4cf37edb5eb924fc517
TNTSuperMan <abworld@outlook.jp>
2025-05-14 00:36:27 +0900

    rehookとweakmapで効率化

--- packages/rjs/src/reactivity/hook.ts
+++ packages/rjs/src/reactivity/hook.ts
@@ -1,12 +1,20 @@
-export const hook_states: [symbol, ()=>void, (()=>void)?][] = [];
+export const hook_funcs = new WeakMap<WeakKey, [()=>void, (()=>void)?]>();
+export const hook_states: symbol[] = [];
 export const available_hooks = new WeakSet();
 
-export const hook = (target: ()=>void, effect?: ()=>void) => {
-    const id = Symbol();
+export const rehook = (id: symbol) => {
+    const [target, effect] = hook_funcs.get(id) ?? [];
+    if(!target) throw new Error("Receive invaild rehook id");
     available_hooks.add(id);
-    hook_states.push([id, target, effect]);
+    hook_states.push(id);
     target();
     hook_states.pop();
     effect?.();
+}
+
+export const hook = (target: ()=>void, effect?: ()=>void) => {
+    const id = Symbol();
+    hook_funcs.set(id, [target, effect]);
+    rehook(id);
     return id;
 }
--- packages/rjs/src/reactivity/reactive.ts
+++ packages/rjs/src/reactivity/reactive.ts
@@ -1,13 +1,13 @@
-import { available_hooks, hook, hook_states } from "./hook";
+import { available_hooks, hook_states, rehook } from "./hook";
 
 export const reactive = <T extends object>(target: T): T => {
-    let deps: [PropertyKey, symbol, ()=>void, (()=>void)?][] = [];
+    let deps: [PropertyKey, symbol][] = [];
     const child_proxies = new Map<PropertyKey, object>();
     const proxy = new Proxy(target, {
         get(target, prop, receiver){
-            const last_hs = hook_states.at(-1);
-            if(last_hs)
-                deps.push([prop, ...last_hs]);
+            const last_id = hook_states.at(-1);
+            if(last_id)
+                deps.push([prop, last_id]);
             const value = Reflect.get(target, prop, receiver) as unknown;
             if(typeof value == "object" && value)
                 if(child_proxies.has(prop)){
@@ -24,7 +24,7 @@ export const reactive = <T extends object>(target: T): T => {
             deps = deps.filter(e=>available_hooks.has(e[1]));
             deps.filter(e=>e[0] == prop).forEach(e=>{
                 available_hooks.delete(e[1]);
-                hook(e[2], e[3]);
+                rehook(e[1]);
             });
             return setres;
         }
