1b97387c9743814209569ff4165422435e05b7b8
TNTSuperMan <abworld@outlook.jp>
2025-05-14 01:42:16 +0900

    reactiveの仕組み変える

--- packages/rjs/src/reactivity/hook.ts
+++ packages/rjs/src/reactivity/hook.ts
@@ -1,12 +1,15 @@
 export const hook_funcs = new WeakMap<WeakKey, [()=>void, (()=>void)?]>();
 export const hook_states: symbol[] = [];
-export const available_hooks = new WeakSet;
 
 export const rehook = (id: symbol) => {
-    const [target, effect] = hook_funcs.get(id) ?? [];
-    if(!target) throw new Error("Receive invaild rehook id");
-    available_hooks.add(id);
-    hook_states.push(id);
+    if(!hook_funcs.has(id)) return;
+    const [target, effect] = hook_funcs.get(id)!;
+
+    hook_funcs.delete(id);
+    const new_id = Symbol();
+    hook_funcs.set(new_id, [target, effect]);
+
+    hook_states.push(new_id);
     target();
     hook_states.pop();
     effect?.();
@@ -16,5 +19,4 @@ export const hook = (target: ()=>void, effect?: ()=>void) => {
     const id = Symbol();
     hook_funcs.set(id, [target, effect]);
     rehook(id);
-    return id;
 }
--- packages/rjs/src/reactivity/reactive.ts
+++ packages/rjs/src/reactivity/reactive.ts
@@ -1,13 +1,14 @@
-import { available_hooks, hook_states, rehook } from "./hook";
+import { hook_states, rehook } from "./hook";
 
 export const reactive = <T extends object>(target: T): T => {
-    let deps: [PropertyKey, symbol][] = [];
+    const deps = new Map<PropertyKey, symbol[]>();
     const child_proxies = new Map<PropertyKey, object>();
     const proxy = new Proxy(target, {
         get(target, prop, receiver){
             const last_id = hook_states.at(-1);
             if(last_id)
-                deps.push([prop, last_id]);
+                if(!deps.has(prop)) deps.set(prop, [last_id]);
+                else deps.get(prop)!.push(last_id);
             const value = Reflect.get(target, prop, receiver) as unknown;
             if(typeof value == "object" && value)
                 if(child_proxies.has(prop)){
@@ -21,11 +22,7 @@ export const reactive = <T extends object>(target: T): T => {
         },
         set(target, prop, val, receiver){
             const setres = Reflect.set(target, prop, val, receiver);
-            deps = deps.filter(e=>available_hooks.has(e[1]));
-            deps.filter(e=>e[0] == prop).forEach(e=>{
-                available_hooks.delete(e[1]);
-                rehook(e[1]);
-            });
+            (deps.get(prop) ?? []).splice(0).forEach(e=>rehook(e));
             return setres;
         }
     });
